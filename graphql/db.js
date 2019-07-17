c6e95f140108c9be0f79386e6eb48b988fbd98f0
utober <utober77@gmail.com>
2019-07-17 15:32:34 +0900

    correct an error

--- graphql/db.js
+++ graphql/db.js
@@ -4,7 +4,7 @@ const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
 const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
 const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
-export const getMoives = async (limit, rating) => {
+export const getMovies = async (limit, rating) => {
   const {
     data: {
       data: { movies }
